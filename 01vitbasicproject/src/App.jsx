71a5ca9256e62c9a2ea886b88a84ce8aeabc684f
AkshayKothule <akshaykothule2002@gmail.com>
2024-09-16 18:20:38 +0530

    update the project

--- 01vitbasicproject/src/App.jsx
+++ 01vitbasicproject/src/App.jsx
@@ -1,10 +1,13 @@
 
+import Chai from "./chai";
 function App() {
   
 
   return (
-  
+   <>
+    <Chai/>
     <h1>Chai Aur React with Vite | AK</h1>
+    </>
   );
 }
 
---
+++ 01vitbasicproject/src/chai.jsx
@@ -0,0 +1,8 @@
+
+function Chai(){
+    return(
+        <h2>chai is Ready</h2>
+    )
+}
+
+export default Chai
